1296ee6c3cfe8d8a15acb6b9eff10f49d2347d12
Kendaii <charles.censier@gmail.com>
2021-04-29 15:17:29 +0200

    FizzBuzz 1 to N

--- fizzbuzz/js/src/app.js
+++ fizzbuzz/js/src/app.js
@@ -1,13 +1,33 @@
 // Impement fizzbuzz logic
-function fizzbuzz(number) {
-  return '0';
+function	fizzbuzz( number ) 
+{
+	if ( number <= 0 )
+		return "";
+	else if ( number % 15 === 0 )
+		return "FizzBuzz";
+	else if ( number % 3 === 0 )
+		return "Fizz";
+	else if ( number % 5 === 0 )
+		return "Buzz";
+	else
+		return number.toString();
 }
 
 // Implement the wrapper so we can count from 0 to STOP.
-function main() {
-   for(let i=0; i<10;i++) {
-     console.log(fizzbuzz(i));
-   }
+function 	main() 
+{
+	let 	N 	= 0;
+
+	if ( process.argv.length > 2 ) {
+		N = process.argv[2];
+	}
+	else {
+		N = 10;
+	}
+
+	for ( let i = 1; i <= N ; i++ ) {
+		console.log( fizzbuzz( i ) );
+	}
 }
 
 main();
